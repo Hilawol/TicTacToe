c46dd3b1d6744e8e78f7dbf6bacfa171ef33a2c9
Hila <hila.kershenovich@solarwinds.com>
2022-01-04 14:39:34 +0200

    Added styling with emotion/css to Square.tsx

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import {values} from "../Board/Board";
-import './square.css';
+import { css,cx } from '@emotion/css'
+
+const squareStyle = css`
+  background-color: transparent;
+  border: 1px solid black;
+`
+const winnerStyle = css`
+  color: red;
+`
+const gameOverStyle = css`
+  background-color: lightgray;
+`
+
 
 interface Props {
     onSquareClick (e: React.MouseEvent<HTMLButtonElement>): void;
@@ -18,7 +30,7 @@ const Square = ({onSquareClick, id,value,winner,gameOver} : Props) =>{
         onSquareClick(e);
     }
 
-    return (<button type="button" className={ winner?  "winner" : gameOver? "gameover" : "btn"}  id={id} onClick={onClick}>{value}</button>);
+    return (<button type="button" className={ winner? cx(winnerStyle,squareStyle) : gameOver?cx(gameOverStyle,squareStyle)  : squareStyle}  id={id} onClick={onClick}>{value}</button>);
 }
 
 export default Square;
